2ec40c2b1c31a6882ef4e148e3afcb2d2ed174b4
joaovitorflemes <158162525+joaovitorflemes@users.noreply.github.com>
2024-07-25 12:27:37 +0000

    atual

--- exercicioss.py
+++ exercicioss.py
@@ -1,16 +1,29 @@
-ord = [5,6,69,55.666,'lavar','chute',55]
-print(ord)
-ord.append() #adc numero a lista
-ord.extend({}) #adc varios numeros a lista
-ord.insert() #adc elementos em uma posiçao escolida ou em posiçao aleatoria 
-for elemento in ord: # mostra elementos
-    print(elemento)
+#ord = [5,6,69,55.666,'lavar','chute',55]
+#print(ord[6])
+#ord.append() #adc numero a lista
+#ord.extend({}) #adc varios numeros a lista
+#ord.insert() #adc elementos em uma posiçao escolido ou em posiçao aleatoria 
+#for elemento in ord: # mostra elementos
+ #   print(elemento)
 
-clear() #remove todos os itens da lista
-copy() #faz uma copia dos valores da lista
-count(valor) #retorna quantas vezes o valor informado ocorre na lista
-index(valor) #retorna posiçao na lista que o valor foi encontrado
-pop(index) #remove e retorna o item localizado na posiçao informada
-remove(valor) #remove a primeira ocorrencia o valor informado 
-reverse() # inverte a ordem da lista
-sort() #ordena a lista
+#clear() #remove todos os itens da lista
+#copy() #faz uma copia dos valores da lista
+#count(valor) #retorna quantas vezes o valor informado ocorre na lista
+#index(valor) #retorna posiçao na lista que o valor foi encontrado
+#pop(index) #remove e retorna o item localizado na posiçao informada
+#remove(valor) #remove a primeira ocorrencia o valor informado 
+#reverse() # inverte a ordem da lista
+#sort() #ordena a lista
+
+#matriz = [[1,3,4],[4,2,5]]
+#for linha in matriz:
+ #   print(linha)
+
+#a = {5,6,7,8,9}
+#b = {3,4,5,6}
+#print(a|b)
+#print(a-b)
+#print(a&b)
+#print(a^b)
+
+
